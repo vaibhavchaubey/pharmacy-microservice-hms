9563f67b442eed5311c4137593ac5c76c59bbd1b
Vaibhav <vaibhavchaubey786@gmail.com>
2025-09-21 23:26:55 +0530

    Initial commit

---
+++ src/main/java/com/hms/pharmacy/PharmacyMicroserviceHmsApplication.java
@@ -0,0 +1,13 @@
+package com.hms.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyMicroserviceHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyMicroserviceHmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hms/pharmacy/PharmacyMicroserviceHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyMicroserviceHmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
