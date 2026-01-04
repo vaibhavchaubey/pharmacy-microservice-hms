bd4b7bd5fe81718ead5eff94491df30b8fa92d65
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-04 13:19:56 +0530

    Refactor batch details formatting in sellStock method to improve clarity

--- src/main/java/com/hms/pharmacy/service/MedicineInventoryServiceImpl.java
+++ src/main/java/com/hms/pharmacy/service/MedicineInventoryServiceImpl.java
@@ -120,13 +120,13 @@ public class MedicineInventoryServiceImpl implements MedicineInventoryService {
             Integer availableQuantity = inventory.getQuantity();
             if (availableQuantity <= remainingQuantity) {
                 // Use up the entire batch
-                batchDetails.append(String.format("Batch %s : %d units\n", inventory.getBatchNo(), availableQuantity));
+                batchDetails.append(String.format("%s : %d units\n", inventory.getBatchNo(), availableQuantity));
                 remainingQuantity -= availableQuantity;
                 inventory.setQuantity(0);
                 inventory.setStatus(StockStatus.EXPIRED);
             } else {
                 // Use part of the batch
-                batchDetails.append(String.format("Batch %s : %d units\n", inventory.getBatchNo(), remainingQuantity));
+                batchDetails.append(String.format("%s : %d units\n", inventory.getBatchNo(), remainingQuantity));
                 inventory.setQuantity(availableQuantity - remainingQuantity);
                 remainingQuantity = 0;
             }
